7375e5eb0ac475c0925175763f7b0d87b9fccfbc
Alex Crichton <alex@alexcrichton.com>
2019-03-15 13:56:46 -0700

    Migrate to the 2018 edition
    No need for `extern crate` imports!

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,15 +1,9 @@
-#[macro_use]
-extern crate cfg_if;
-extern crate web_sys;
-extern crate wasm_bindgen;
-
 use wasm_bindgen::prelude::*;
 
-cfg_if! {
+cfg_if::cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function to get better error messages if we ever panic.
     if #[cfg(feature = "console_error_panic_hook")] {
-        extern crate console_error_panic_hook;
         use console_error_panic_hook::set_once as set_panic_hook;
     } else {
         #[inline]
@@ -17,11 +11,10 @@ cfg_if! {
     }
 }
 
-cfg_if! {
+cfg_if::cfg_if! {
     // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
     // allocator.
     if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
     }
