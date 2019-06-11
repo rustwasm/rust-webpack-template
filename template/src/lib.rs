b2b8b8b48a125dfab4f9d0f9cb5cc3bf478b6895
Pauan <pcxunlimited@gmail.com>
2019-06-11 11:41:52 +0200

    Minor tweak to lib.rs

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -19,6 +19,8 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
+
+    // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
 
     Ok(())
