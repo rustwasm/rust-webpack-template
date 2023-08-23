4fb1e11e42ebea024fab9f0e687efd17d3d8e612
matthewhammer <matthew@dfinity.org>
2023-08-23 09:54:44 -0600

    draw a circle on a canvas, from Rust Wasm module.

--- template/tests/app.rs
+++ template/tests/app.rs
@@ -1,10 +1,9 @@
-use wasm_bindgen_test::{wasm_bindgen_test_configure, wasm_bindgen_test};
 use futures::prelude::*;
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 
 wasm_bindgen_test_configure!(run_in_browser);
-
 
 // This runs a unit test in native Rust, so it can only use Rust APIs.
 #[test]
@@ -12,13 +11,11 @@ fn rust_test() {
     assert_eq!(1, 1);
 }
 
-
 // This runs a unit test in the browser, so it can use browser APIs.
 #[wasm_bindgen_test]
 fn web_test() {
     assert_eq!(1, 1);
 }
-
 
 // This runs a unit test in the browser, and in addition it supports asynchronous Future APIs.
 #[wasm_bindgen_test(async)]
@@ -28,8 +25,7 @@ fn async_test() -> impl Future<Item = (), Error = JsValue> {
 
     // Converts that Promise into a Future.
     // The unit test will wait for the Future to resolve.
-    JsFuture::from(promise)
-        .map(|x| {
-            assert_eq!(x, 42);
-        })
+    JsFuture::from(promise).map(|x| {
+        assert_eq!(x, 42);
+    })
 }
